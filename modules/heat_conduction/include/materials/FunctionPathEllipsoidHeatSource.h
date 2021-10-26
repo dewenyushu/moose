4d5c4a28420c9ab90eef16218ccb8a2ecc89dd5b
dewenyushu <dewen.yushu@inl.gov>
2021-10-26 14:55:38 -0600

    Point, line and mixed heat source

--- modules/heat_conduction/include/materials/FunctionPathEllipsoidHeatSource.h
+++ modules/heat_conduction/include/materials/FunctionPathEllipsoidHeatSource.h
@@ -11,6 +11,7 @@
 
 #include "Material.h"
 #include "Function.h"
+#include "MooseEnum.h"
 
 /**
  * Double ellipsoid heat source distribution.
@@ -25,6 +26,14 @@ public:
 protected:
   virtual void computeQpProperties() override;
 
+  Real computeHeatSourceAtTime(const Real x, const Real y, const Real z, const Real time);
+
+  Real computeAveragedHeatSource(
+      const Real x, const Real y, const Real z, const Real time_begin, const Real time_end);
+
+  Real computeMixedHeatSource(
+      const Real x, const Real y, const Real z, const Real time_begin, const Real time_end);
+
   /// power
   const Real _P;
   /// process efficienty
@@ -37,6 +46,11 @@ protected:
   const Function & _function_x;
   const Function & _function_y;
   const Function & _function_z;
+
+  /// type of heat source
+  const enum class HeatSourceType { POINT, LINE, MIXED } _heat_source_type;
+
+  const Real _threshold_length;
 
   ADMaterialProperty<Real> & _volumetric_heat;
 };
