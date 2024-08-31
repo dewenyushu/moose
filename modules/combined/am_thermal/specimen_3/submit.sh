4ab8410e41430a474026ed86214b0aa7fadf2812
dewen-yushu <dewen.yushu@inl.gov>
2024-08-30 17:15:46 -0700

    Modify job script

--- modules/combined/am_thermal/specimen_3/submit.sh
+++ modules/combined/am_thermal/specimen_3/submit.sh
@@ -2,14 +2,18 @@
 #PBS -m abe
 #PBS -M dewen.yushu@inl.gov
 #PBS -N case_3
-#PBS -l select=8:ncpus=48:mpiprocs=48
+#PBS -l select=3:ncpus=48:mpiprocs=48
+#PBS -l place=scatter:excl
 #PBS -l walltime=100:00:00
 #PBS -P moose
 
+JOB_NUM=${PBS_JOBID%%\.*}
+
 cd $PBS_O_WORKDIR
+
 module purge
-conda init
+source activate base
 conda activate moose
 
-mpirun ~/projects/moose-am/modules/combined-opt -i thermal.i
+mpiexec /home/yushdewe/projects/moose-am/modules/combined/combined-opt -i thermal.i
 
