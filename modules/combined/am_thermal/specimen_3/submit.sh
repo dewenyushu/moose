dd2f98ee25a5400605da2b69b1cd1de34d52a759
dewen-yushu <dewen.yushu@inl.gov>
2024-09-03 10:53:10 -0700

    only use 1 core

--- modules/combined/am_thermal/specimen_3/submit.sh
+++ modules/combined/am_thermal/specimen_3/submit.sh
@@ -2,7 +2,7 @@
 #PBS -m abe
 #PBS -M dewen.yushu@inl.gov
 #PBS -N case_3
-#PBS -l select=3:ncpus=48:mpiprocs=48
+#PBS -l select=1:ncpus=48:mpiprocs=48
 #PBS -l place=scatter:excl
 #PBS -l walltime=100:00:00
 #PBS -P moose
