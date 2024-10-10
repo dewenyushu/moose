6a6c93ed8bda124eeecf717d10c74f498ce57819
dewen-yushu <dewen.yushu@inl.gov>
2024-10-10 12:46:35 -0700

    Add inputs for honeycomb cases

---
+++ modules/combined/honeycomb_tensile_Oct_2024/048_120mmH_11.008mmGW_6mmCW_Custom_Square_Honeycomb_3mmLP_2000umLWT_500umBR_trunc18/submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -m abe
+#PBS -M dewen.yushu@inl.gov
+#PBS -N 048
+#PBS -l select=1:ncpus=48:mpiprocs=48
+#PBS -l place=scatter:excl
+#PBS -l walltime=100:00:00
+#PBS -P moose
+
+JOB_NUM=${PBS_JOBID%%\.*}
+
+cd $PBS_O_WORKDIR
+
+module purge
+source activate base
+conda activate moose
+
+mpiexec /home/yushdewe/projects/moose_light_weight_ldrd/modules/combined/combined-opt -i input_disp_control_plastic.i --recover
+
